e0154a5a952b57e073c6a8cf9885d83ed05c3ebf
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2021-10-19 15:15:08 -0400

    Create profile picture functionality

--- FrontEnd/valen/src/App.js
+++ FrontEnd/valen/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import ProfilePicture from './components/ProfilePicture/ProfilePicture';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ProfilePicture />
     </div>
   );
 }
