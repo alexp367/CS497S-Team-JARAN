213cb88f6866bd5a1c54c3cfe6fe6d5788f43c5a
NamPhan56 <namp@umass.edu>
2021-10-21 02:56:49 -0400

    stopping for the night, pushing up progress

--- FrontEnd/valen/src/App.js
+++ FrontEnd/valen/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import ProfilePicture from './components/ProfilePicture/ProfilePicture';
+//import { BroswerRouter as Router, Route, Switch} from "react-router-dom";
+
+
 
 function App() {
   return (
