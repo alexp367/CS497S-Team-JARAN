8fb00d9a1da856338676e1816e2863ff0b0f56fe
NamPhan56 <namp@umass.edu>
2021-10-22 01:16:45 -0400

    restructured ProfilePicture to be nested in profile

--- FrontEnd/valen/src/App.js
+++ FrontEnd/valen/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
-import ProfilePicture from './components/ProfilePicture/ProfilePicture';
+import Profile from "./components/profile/profile"
 //import { BroswerRouter as Router, Route, Switch} from "react-router-dom";
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <ProfilePicture />
+      <Profile />
     </div>
   );
 }
