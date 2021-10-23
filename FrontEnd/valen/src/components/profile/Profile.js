879462ab37f78934c4e1b775629d336928e83c7e
NamPhan56 <namp@umass.edu>
2021-10-23 02:28:11 -0400

    renamed profile.js to Profile.js, capitalization may cause problems for programmers using Windows

---
+++ FrontEnd/valen/src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import ProfilePicture from './ProfilePicture/ProfilePicture';
+
+const Profile = () => {
+
+    return (
+        <div>
+        <ProfilePicture />
+            
+        </div>
+    )
+}
+
+export default Profile;
