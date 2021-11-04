85effec89ebeca5006432ce52095907fad5b4ca8
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2021-11-04 17:55:20 -0400

    Integrate MongoDB. Begin storing profiles in DB

--- FrontEnd/valen/src/components/profile/AboutMe/AboutMe.js
+++ FrontEnd/valen/src/components/profile/AboutMe/AboutMe.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './AboutMe.css';
 
 class AboutMe extends Component {
+    saveProfile() {
+        const profile = {"myGender": document.getElementById("myGender").value,
+                     "preferredGender": document.getElementById("preferredGender").value,
+                     "myAge": document.getElementById("myAge").value,
+                     "preferredAge": document.getElementById("preferredAge").value,
+                     "activities": document.getElementById("activities").value,
+                     "movies": document.getElementById("movies").value,
+                     "description": document.getElementById("description").value,
+                    };
+
+        fetch("./save", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(profile),
+        });
+    }
+
     render() {
         return (
             <div>
@@ -22,12 +41,12 @@ class AboutMe extends Component {
                                 <option value="Both">Both</option>
                                 <option value="Other">Other</option>
                             </select>
-                            <textarea name="description" class="description" cols="90" rows="6" maxlength="200" placeholder="In my own words..."></textarea>
+                            <textarea name="description" id="description" class="description" cols="90" rows="6" maxLength="200" placeholder="In my own words..."></textarea>
                             <br></br>
                             <label for="myAge">I am </label>
                             <input type="number" id="myAge" name="myAge" min="18"></input> years old,
-                            <label for="desiredAge"> looking for someone within </label>
-                            <input type="number" id="desiredAge" name="desiredAge" min="0"></input> years of my age
+                            <label for="preferredAge"> looking for someone within </label>
+                            <input type="number" id="preferredAge" name="preferredAge" min="0"></input> years of my age
                             <br></br>
                             Some activities I like are <input type="text" id="activities" name="activities" size="60"></input>
                             <br></br>
@@ -37,7 +56,9 @@ class AboutMe extends Component {
 
 
                             <br></br><br></br>
-                            <input type="submit" value="Save"></input>
+                            <form action="save" method="post">
+                                <input type="submit" value="Save" onClick={this.saveProfile}></input>
+                            </form>
                         </form>
                     </div>
                 </div>
