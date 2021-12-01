c8d0ae81641b28b1d5869b14b6f2c9afe0a50e1a
Jason Canterbury <jcanterbury@umass.edu>
2021-12-01 15:43:41 -0500

    Dockerization of all microservices except APIGateway

--- TextChat/index.js
+++ TextChat/index.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3003
 const io = require('socket.io')(server)
 const path = require('path')
 
